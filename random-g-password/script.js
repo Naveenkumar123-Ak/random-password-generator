29ab0c53e433d8532d6522585016641d56c2acf6
Naveenkumar123-Ak <114437341+Naveenkumar123-Ak@users.noreply.github.com>
2024-04-27 18:50:48 +0530

    Add files via upload

---
+++ random-g-password/script.js
@@ -0,0 +1,25 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "@#$%^&*()_+~|}{[]></-=";
+const allChar = upperCase + lowerCase + numbers + symbols;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+
+  while (lenght > password.length) {
+    password += allChar[Math.floor(Math.random() * allChar.length)];
+  }
+  passwordBox.value = password;
+}
+function copy() {
+  document.execCommand("copy");
+  alert("Password copied successfully");
+}
